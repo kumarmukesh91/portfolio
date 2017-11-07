595963bdf5e4651a7f63f44d4b92bf5fcb87c802
aks <ankit.singhaniyaz@gmail.com>
2017-11-07 22:56:25 +0530

    attempt to load css via cdn for avoiding flash of content

--- pages/index.vue
+++ pages/index.vue
@@ -286,7 +286,7 @@
 <script>
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
 
 import Logo from '~/components/Logo.vue'
 
